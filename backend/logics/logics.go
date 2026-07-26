9dd7c9fb8ac3c5dc0fa941aaa98a2eed0cb08c6d
rpavlov <ripavlovvv@gmail.com>
2026-07-26 15:47:49 +0500

    feat: new logics with mapping

--- backend/logics/logics.go
+++ backend/logics/logics.go
@@ -10,13 +10,12 @@ import (
 
 const baseURL = "https://groupietrackers.herokuapp.com/api"
 
-func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
+func MainHandler(w http.ResponseWriter, r *http.Request) {
 	var index models.Index
 
 	if err := api.FetchJSON(baseURL, &index); err != nil {
 		log.Println(err)
 		http.Error(w, "Ошибка получения данных API", http.StatusBadRequest)
-		// return fmt.Errorf("error occured while fetching json from index: %e", err)
 		return
 
 	}
@@ -26,21 +25,63 @@ func ArtistsHandler(w http.ResponseWriter, r *http.Request) {
 	if err := api.FetchJSON(index.Artists, &artists); err != nil {
 		log.Println(err)
 		http.Error(w, "Ошибка получения данных API", http.StatusInternalServerError)
-		// return fmt.Errorf("error occured while fetching json from artists: %e", err)
+
 		return
+	}
+
+	var location models.LocationsResponse
+
+	if err := api.FetchJSON(index.Locations, &location); err != nil {
+		log.Println(err)
+		http.Error(w, "Ошибка получения данных API", http.StatusInternalServerError)
+
+		return
+	}
+
+	var dates models.DatesResponse
+
+	if err := api.FetchJSON(index.Dates, &dates); err != nil {
+		log.Println(err)
+		http.Error(w, "Ошибка получения данных API", http.StatusInternalServerError)
+
+		return
+	}
+
+	datesmap := make(map[int][]string)
+	locationmap := make(map[int][]string)
+	for _, d := range dates.Index {
+		datesmap[d.ID] = d.Dates
+	}
+	for _, x := range location.Index {
+		locationmap[x.ID] = x.Locations
+	}
+
+	views := make([]models.ArtistView, 0, len(artists))
+
+	for _, a := range artists {
+		views = append(views,
+			models.ArtistView{
+				Artist:    a,
+				Locations: locationmap[a.ID],
+				Dates:     datesmap[a.ID],
+			})
 	}
 
 	tmp, err := template.ParseFiles("frontend/templates/artists.html")
 
 	if err != nil {
 		http.Error(w, "Ошибка парсинга", http.StatusInternalServerError)
-		// return fmt.Errorf("error occured while parsing template: %e", err)
 		return
 	}
 
-	if err := tmp.Execute(w, artists); err != nil {
+	// if err := tmp.Execute(w, artists); err != nil {
+	// 	log.Println(err)
+
+	// 	return
+	// }
+	if err := tmp.Execute(w, views); err != nil {
 		log.Println(err)
-		// return fmt.Errorf("error occured while executing information: %e", err)
+
 		return
 	}
 
