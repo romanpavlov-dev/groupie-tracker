be72314dcc6787150d84e2d70337d1ae9e9be6c6
rpavlov <ripavlovvv@gmail.com>
2026-07-26 15:48:16 +0500

    feat: new model

--- backend/models/models.go
+++ backend/models/models.go
@@ -7,13 +7,19 @@ type Index struct {
 	Relation  string `json:"relation"`
 }
 
+type ArtistView struct {
+	Artist
+	Locations []string
+	Dates     []string
+}
+
 type Artist struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
-	FirstAlbum  string   `json:"firstAlbum"`
+	FirstAlbum   string   `json:"firstAlbum"`
 	Locations    string   `json:"locations"`
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
